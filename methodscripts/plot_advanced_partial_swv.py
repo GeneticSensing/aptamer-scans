ede44340a205b7085a0d9d62649a93e83221fc7e
Adam Mak <maka9@mcmaster.ca>
2024-10-29 15:34:55 -0400

    First working draft of partial SWV

--- methodscripts/plot_advanced_partial_swv.py
+++ methodscripts/plot_advanced_partial_swv.py
@@ -10,34 +10,6 @@ baseline and peak, which is computed with scipy. Scanning windows are 30mV.
 
 When the experiement is completed, i.e. all scans are completed, the number
 of scans needs to be manually reset to 0.
-
--------------------------------------------------------------------------------
-Copyright (c) 2019-2021 PalmSens BV
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-   - Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-   - Neither the name of PalmSens BV nor the names of its contributors
-     may be used to endorse or promote products derived from this software
-     without specific prior written permission.
-   - This license does not release you from any requirement to obtain separate
-     licenses from 3rd party patent holders to use this software.
-   - Use of the software either in source or binary form must be connected to,
-     run on or loaded to an PalmSens BV component.
-
-DISCLAIMER: THIS SOFTWARE IS PROVIDED BY PALMSENS "AS IS" AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
-EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 # Standard library imports
@@ -61,9 +33,9 @@ import palmsens.mscript
 import palmsens.serial
 
 
-###############################################################################
-# Start of configuration
-###############################################################################
+### Start of configuration
+
+LOG = logging.getLogger(__name__)
 
 # COM port of the device (None = auto detect).
 DEVICE_PORT = None
@@ -76,19 +48,6 @@ PARTIAL_SWV_ESPICO_PATH = 'scripts/partial_swv_espico.mscr'
 # Location of output files. Directory will be created if it does not exist.
 OUTPUT_PATH = 'output'
 
-# In this example, columns refer to the separate "pck_add" entries in each
-# MethodSCRIPT data package. For example, in the used script there are
-# four columns per data package:
-#   pck_start
-#   pck_add p
-#   pck_add c
-#   pck_add f
-#   pck_add r
-#   pck_end
-# Here, the indices 0/1/2/3 refer to the variables p/c/f/r/ respectively.
-# The following variables define which columns (i.e. variables) to plot, and
-# how they are named in the figure.
-
 # Column names.
 COLUMN_NAMES = ['Potential', 'Current', 'Forward Current', 'Reverse Current']
 # Index of column to put on the x axis.
@@ -96,13 +55,7 @@ XAXIS_COLUMN_INDEX = 0
 # Indices of columns to put on the y axis. The variables must be same type.
 YAXIS_COLUMN_INDICES = [1, 2, 3]
 
-###############################################################################
-# End of configuration
-###############################################################################
-
-
-LOG = logging.getLogger(__name__)
-
+### End of configuration
 
 def write_curves_to_csv(file: typing.IO, curves: list[list[list[palmsens.mscript.MScriptVar]]]):
     """Write the curves to file in CSV format.
@@ -156,38 +109,13 @@ def update_method_script(template_path: str, dest_path: str, replacements: dict)
         content = f.read()
     for placeholder, value in replacements.items():
         content = content.replace(placeholder, value)
-    with open(dest_path, "w") as f:
+    with open(dest_path, "w", newline='\n') as f:
         f.write(content)
-
-
-class PalmSensDevice:
-    def __init__(self, port=None):
-        self.port = port or palmsens.serial.auto_detect_port()
-        self.device = None
-
-    def __enter__(self):
-        self.comm = palmsens.serial.Serial(self.port, 1)
-        self.device = palmsens.instrument.Instrument(self.comm)
-
-        device_type = self.device.get_device_type()
-        if device_type != palmsens.instrument.DeviceType.EMSTAT_PICO:
-            self.comm.close()
-            raise RuntimeError("Device is not an Emstat Pico")
-
-        LOG.info(f'Connected to {device_type}.')
-        return self
-
-    def __exit__(self):
-        self.comm.close()
-
-    def send_script(self, script_path):
-        LOG.info('Sending MethodSCRIPT.')
-        self.device.send_script(script_path)
-        return self.device.readlines_until_end()
     
 
 class ScanTracker:
-    """Scan tracking data is persisted in scan_tracker.json
+    """
+    Scan tracking data is persisted in scan_tracker.json
     
     Everytime an experiment completes, i.e. all scans completes, num_scans must be manually reset to 0
     """
@@ -203,26 +131,29 @@ class ScanTracker:
         return self.data["num_scans"] % 10 == 0
 
     def update_peak_values(self, xvalues, yvalues):
-        prominence_threshold = 0.15
+        trunc_xvalues = xvalues[100:]
+        trunc_yvalues = yvalues[100:]
+        prominence_threshold = 0.0000005
         distance_threshold = 100
-        peaks, properties = sg.find_peaks(yvalues, prominence=prominence_threshold, distance=distance_threshold)
+        peaks, properties = sg.find_peaks(trunc_yvalues, prominence=prominence_threshold, distance=distance_threshold)
         if len(peaks) == 0:
             print("No peaks found!")
             return
         if len(peaks) > 1:
             print("More than one peak found!")
-        self.data["peak"] = xvalues[peaks[0]]
-        self.data["left_baseline"] = xvalues[properties['left_bases'][0]]
+        self.data["peak"] = trunc_xvalues[peaks[0]]
+        self.data["left_baseline"] = trunc_xvalues[properties['left_bases'][0]]
         save_json(self.data, self.file_path)
 
     def get_replacements(self):
         left_baseline = self.data["left_baseline"]
         peak = self.data["peak"]
+        # Determine scanning windows
         return {
-            "<E_begin_baseline>": f"{left_baseline - 30}m",
-            "<E_end_baseline>": f"{left_baseline}m",
-            "<E_begin>": f"{peak - 15}m",
-            "<E_end>": f"{peak + 15}m"
+            "<E_begin_baseline>": f"{int(left_baseline*1000) - 30}m",
+            "<E_end_baseline>": f"{int(left_baseline*1000)}m",
+            "<E_begin_peak>": f"{int(peak*1000) - 15}m",
+            "<E_end_peak>": f"{int(peak*1000) + 15}m"
         }
 
 
@@ -247,10 +178,11 @@ def plot_curves(curves: list[list[list[palmsens.mscript.MScriptVar]]], base_path
     color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
     # Loop through all curves
+    print(len(curves))
     for icurve, curve in enumerate(curves):
         xvalues = palmsens.mscript.get_values_by_column(curves, XAXIS_COLUMN_INDEX, icurve)
 
-        for y_axis in YAXIS_COLUMN_INDICES:
+        for iy_axis, y_axis in enumerate(YAXIS_COLUMN_INDICES):
             yvalues = palmsens.mscript.get_values_by_column(curves, y_axis, icurve)
 
             if curve[0][y_axis].type != yvar.type:
@@ -259,26 +191,20 @@ def plot_curves(curves: list[list[list[palmsens.mscript.MScriptVar]]], base_path
             # Create a label for each curve
             label = f'{COLUMN_NAMES[y_axis]} vs {COLUMN_NAMES[XAXIS_COLUMN_INDEX]}'
             if len(curves) > 1:
-                label += f' {icurve}'
+                label += ' cell ' + ('off' if icurve % 2 == 0 else 'on')
+                label += ' (baseline)' if icurve < 2 else ' (peak)'
 
             # Handle partial plot (two segments)
             if partial:
-                diffs = np.diff(xvalues)
-                split_index = np.argmax(diffs) + 1  # Find the largest gap between points
-
-                # Split the data into baseline and peak regions
-                xvalues_baseline, xvalues_peak = xvalues[:split_index], xvalues[split_index:]
-                yvalues_baseline, yvalues_peak = yvalues[:split_index], yvalues[split_index:]
-
-                # Plot each segment with the same color for consistency
-                color = color_cycle[icurve % len(color_cycle)]
-                plt.plot(xvalues_baseline, yvalues_baseline, color=color, label=f'{label} (baseline)')
-                plt.plot(xvalues_peak, yvalues_peak, color=color, label=f'{label} (peak)')
+                # Associated baseline and peak curves have same colour
+                color = color_cycle[((icurve * len(YAXIS_COLUMN_INDICES) + iy_axis) % (len(YAXIS_COLUMN_INDICES) * 2)) % len(color_cycle)]
+                plt.plot(xvalues, yvalues, color=color, label=label)
             else:
                 # Standard single plot for calibration
                 plt.plot(xvalues, yvalues, label=label)
                 # Determine peak and baseline for subsequent partial scans
-                scan_tracker.update_peak_values(xvalues, yvalues)
+                if icurve == 1 and iy_axis == 0:
+                    scan_tracker.update_peak_values(xvalues, yvalues)
 
     # Display the legend and save the plot
     plt.legend()
@@ -287,21 +213,42 @@ def plot_curves(curves: list[list[list[palmsens.mscript.MScriptVar]]], base_path
 
 
 def perform_scan(script_path: str, scan_tracker: ScanTracker, partial=False):
-    LOG.info("Starting " + "partial" if partial else "calibration" + " SWV scan.")
+    LOG.info("Starting " + ("partial" if partial else "calibration") + " SWV scan.")
     base_path = create_output_path(datetime.datetime.now().strftime('ms_plot_swv_%Y%m%d-%H%M%S'))
 
-    with PalmSensDevice() as device:
-        results = device.send_script(script_path)
-        with open(base_path + '.txt', 'wt') as f:
-            f.writelines(results)
+    port = DEVICE_PORT
+    if port is None:
+        port = palmsens.serial.auto_detect_port()
 
-        curves = palmsens.mscript.parse_result_lines(results)
+    # Create and open serial connection to the device.
+    with palmsens.serial.Serial(port, 1) as comm:
+        device = palmsens.instrument.Instrument(comm)
+        device_type = device.get_device_type()
+        if device_type != palmsens.instrument.DeviceType.EMSTAT_PICO:
+            self.comm.close()
+            raise RuntimeError("Device is not an Emstat Pico")
+        LOG.info('Connected to %s.', device_type)
 
-        with open(base_path + '.csv', 'wt', newline='') as f:
-            write_curves_to_csv(f, curves)
+        # Read and send the MethodSCRIPT file.
+        LOG.info('Sending MethodSCRIPT.')
+        device.send_script(script_path)
 
-        # Call plot_curves with the partial flag
-        plot_curves(curves, base_path, scan_tracker=scan_tracker, partial=partial)
+        # Read the result lines.
+        LOG.info('Waiting for results.')
+        result_lines = device.readlines_until_end()
+
+    os.makedirs(OUTPUT_PATH, exist_ok=True)
+    with open(base_path + '.txt', 'wt', encoding='ascii') as file:
+        file.writelines(result_lines)
+
+    # Parse the result.
+    curves = palmsens.mscript.parse_result_lines(result_lines)
+
+    with open(base_path + '.csv', 'wt', newline='') as f:
+        write_curves_to_csv(f, curves)
+
+    # Call plot_curves with the partial flag
+    plot_curves(curves, base_path, scan_tracker=scan_tracker, partial=partial)
     scan_tracker.increment_scan()
 
 
